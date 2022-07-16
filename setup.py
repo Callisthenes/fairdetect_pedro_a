301ade8ed0907a0edb22157cd25be4abfa20a1df
Pedro V <pedrovesteban@student.ie.edu>
2022-07-16 21:08:52 +0200

    uploading class

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='pythonpackagesample',
+setup(name='fairdetect_pedro_a',
       version='0.1',  # Development release
       description='Python Packaging Starting Kit - Simple Structure to creater your own package - for training purposes!',
       url='https://github.com/manoelgadi/pythonpackagesample',
-      author='Prof. Manoel Gadi',
-      author_email='mfalonso@faculty.ie.edu',
+      author='Pedro V',
+      author_email='pedrovesteban@student.ie.edu',
       license='MIT',
-          packages=['pythonpackagesample'],
+          packages=['fairdetect_pedro_a'],
       zip_safe=False)
